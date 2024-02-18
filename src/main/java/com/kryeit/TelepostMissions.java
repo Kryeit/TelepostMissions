ed8bfd72b1e686c56d18e67ce5d1a17c995c8540
muriplz <46825658+muriplz@users.noreply.github.com>
2024-02-18 20:53:20 +0100

    Fixes

--- src/main/java/com/kryeit/TelepostMissions.java
+++ src/main/java/com/kryeit/TelepostMissions.java
@@ -7,17 +7,10 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
-
 public class TelepostMissions implements DedicatedServerModInitializer {
 
 	public static final String ID = "telepostmissions";
     public static final Logger LOGGER = LoggerFactory.getLogger(ID);
-
-	public static HashMap<UUID, List<UUID>> inviting = new HashMap<>();
-	public static HashMap<UUID, List<UUID>> invited = new HashMap<>();
 
 
 	@Override
--- src/main/java/com/kryeit/mixin/InviteMixin.java
+++ src/main/java/com/kryeit/mixin/InviteMixin.java
@@ -1,12 +1,9 @@
 package com.kryeit.mixin;
 
-import com.kryeit.TelepostMissions;
 import com.kryeit.mission_types.InviteMission;
 import com.kryeit.mission_types.InvitedMission;
 import com.kryeit.telepost.MinecraftServerSupplier;
-import com.kryeit.telepost.Telepost;
 import com.kryeit.telepost.commands.Invite;
-import com.kryeit.telepost.storage.bytes.HomePost;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -15,36 +12,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
 @Mixin(value = Invite.class, remap = false)
 public class InviteMixin {
 
-    @Inject(method = "execute", at = @At("HEAD"))
+    @Inject(method = "execute",
+            at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
     private static void onExecute(CommandContext<ServerCommandSource> context, String name, CallbackInfoReturnable<Integer> cir) {
         ServerPlayerEntity player = context.getSource().getPlayer();
         ServerPlayerEntity invited = MinecraftServerSupplier.getServer().getPlayerManager().getPlayer(name);
 
         if (player == null || invited == null) return;
-
-        Optional<HomePost> home = Telepost.getDB().getHome(player.getUuid());
-        if (home.isEmpty()) return;
-
-        List<UUID> alreadyInvited = TelepostMissions.inviting.get(player.getUuid());
-
-        if (alreadyInvited.contains(invited.getUuid())) return;
-
-        alreadyInvited.add(invited.getUuid());
-        TelepostMissions.inviting.put(player.getUuid(), alreadyInvited);
-
-        List<UUID> alreadyInviting = TelepostMissions.invited.get(invited.getUuid());
-
-        if (alreadyInviting.contains(player.getUuid())) return;
-
-        alreadyInviting.add(player.getUuid());
-        TelepostMissions.invited.put(invited.getUuid(), alreadyInviting);
 
         InviteMission.handleInvite(player.getUuid());
         InvitedMission.handleInvite(invited.getUuid());
